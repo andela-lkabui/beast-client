36446ae668c233fae11607d63794968a68e7ed8d
Lewis Kabui <lewis.maina@andela.com>
2017-01-03 13:42:27 +0300

    Create app view and route

--- oxford_api/views.py
+++ oxford_api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+BASE_URL = 'https://od-api.oxforddictionaries.com/api/v1'
+
+
+def search(request):
+    """The search view."""
+    return HttpResponse('At the index page.')
