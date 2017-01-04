96cdc7bffe5c082bf65848b618337485ca1ae098
Lewis Kabui <lewis.maina@andela.com>
2017-01-04 12:53:36 +0300

    Create multiple settings files for dev and prod environments

--- beast_client/settings/base_settings.py
+++ beast_client/settings/base_settings.py
@@ -19,12 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 's9_kllb=((^yz0m3f7ks!@^2g*$f)q7#gfy9bjkghb-^o6)7ww'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = []
 
 
---
+++ beast_client/settings/prod_settings.py
@@ -0,0 +1,6 @@
+from base_settings import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ['APP_SETTINGS']
+
+DEBUG = False
