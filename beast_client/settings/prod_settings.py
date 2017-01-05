3552ecdcc1ace2ce965cf58adb1dff4b50981bac
Lewis Kabui <lewis.maina@andela.com>
2017-01-05 14:03:23 +0300

    Configure app for deployment to Heroku

--- beast_client/settings/prod_settings.py
+++ beast_client/settings/prod_settings.py
@@ -1,6 +1,6 @@
 from base_settings import *
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['APP_SETTINGS']
+SECRET_KEY = os.environ['SECRET_KEY']
 
 DEBUG = False
